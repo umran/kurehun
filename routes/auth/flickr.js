ad4c78d1a17be0c23084754331c159bca7e40aad
Umran Hussein <umran.hussein@gmail.com>
2016-01-05 08:34:27 -0800

    hopeful

--- routes/auth/flickr.js
+++ routes/auth/flickr.js
@@ -2,6 +2,8 @@ var express = require('express')
 var router = express.Router()
 var passport = require('passport')
 var FlickrStrategy = require('passport-flickr').Strategy
+var EventEmitter = require('events').EventEmitter
+var flow = new Eventemitter()
 
 module.exports = function(fkConf){
 
@@ -10,18 +12,19 @@ module.exports = function(fkConf){
 	    consumerSecret: fkConf.clientSecret,
 	    callbackURL: fkConf.redirectUri
 	  },
-	  function(token, tokenSecret, profile, done) {
-	    console.log(token)
-	    return done(null, token)
+	  function(token, tokenSecret, profile) {
+	    flow.emit('ok', token)
 	  }
-	));
+	))
 
 	router.get('/', passport.authenticate('flickr'), function(req, res){
-	});
+	})
 
 	router.get('/status', passport.authenticate('flickr', { session: false }), function(req, res) {
 		// Successful authentication, redirect home.
-		res.send(req.query)
+		flow.on('ok', function(token){
+			res.send(token)
+		})
 	});
 
 	return router
