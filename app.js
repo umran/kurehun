119945ae8c908c4899e324506985946ecdc421cf
Umran Hussein <umran.hussein@gmail.com>
2016-01-05 07:59:14 -0800

    new attempt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var passport = require('passport')
 var session = require('express-session')
 var RedisStore = require('connect-redis')(session)
 
--- routes/auth/flickr.js
+++ routes/auth/flickr.js
@@ -1,66 +1,31 @@
 var express = require('express')
 var router = express.Router()
-var ExchangeTokens = require('../../api/flickr/src/auth/exchange')
-var Utils = require('../../api/flickr/src/utils')
-var EventEmitter = require("events").EventEmitter
-var flow = new EventEmitter()
+var passport = require('passport')
+var FlickrStrategy = require('passport-flickr').Strategy
 
 module.exports = function(fkConf){
-	var authUser = function(req, res){
 
-		if(!req.session.fk_access_token){
-			var receivedToken = function(err,body) {
-				if(err) {
-					console.log(err);
-				}
-
-				var options = {}
-
-				var response = Utils.parseRestResponse(body);
-				options.user_id = response.user_nsid;
-				options.access_token = response.oauth_token;
-				options.access_token_secret = response.oauth_token_secret;
-				
-				console.log('Yay! Access token is ' + options.access_token)
-				req.session.fk_access_token = options.access_token
-			}
-			
-			var fk = require('../../api/flickr/src/FlickrAPI')
-			var fkOptions = {
-			    api_key: fkConf.clientId,
-			    secret: fkConf.clientSecret,
-			    callback: fkConf.redirectUri
-			}
-		
-			fk.authenticate(fkOptions, res, function(err, options) {
-			
-				if(err){
-					console.log(err)
-					return
-				}
-			
-				flow.on('callback', function(tokens){
-					options.oauth_verifier = tokens.oauth_verifier
-					new ExchangeTokens(options, receivedToken)
-				})
-			
-			})
-		
-			return
-		}
-	
-		console.log('Access token already set in session, no need to authenticate again')
-		res.send('Flickr Authenticated')
-	}
+	passport.use(new FlickrStrategy({
+	    consumerKey: fkConf.clientId,
+	    consumerSecret: fkConf.clientSecret,
+	    callbackURL: fkConf.redirectUri
+	  },
+	  function(token, tokenSecret, profile, done) {
+	    console.log(token)
+	    return done(null, token)
+	  }
+	));
 
 	var handleAuth = function(req, res) {
-		flow.emit('callback', req.query)
-		res.send('Flickr Authentication Almost Done')
+		
 	}
 
-	router.get('/', authUser)
+	router.get('/', passport.authenticate('flickr'));
 
-	router.get('/status', handleAuth)
+	router.get('/status', passport.authenticate('flickr', { session: false }), function(req, res) {
+		// Successful authentication, redirect home.
+		console.log('what?')
+	});
 
 	return router
 }
