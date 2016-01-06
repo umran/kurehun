1b856ad73b1015c9e3e3b743f0fd8775369ec5c2
Umran Hussein <umran.hussein@gmail.com>
2016-01-05 19:48:06 -0800

    cleanup

--- tests/flickr.js
+++ tests/flickr.js
@@ -1,10 +1,13 @@
 var config = require('../config')
 var fk_access_token = '72157660869281453-33a860f63e3ef1b0'
+var user_id = '139026886@N05'
 
 var Flickr = require('flickr').Flickr;
 var fk = new Flickr(config.api.flickr.clientId, config.api.flickr.clientSecret, {"oauth_token": fk_access_token});
 
 var flickr_params = {
+    api_key: config.api.flickr.clientId,
+    user_id: user_id,
     per_page: 25,
     page: 1,
 };
