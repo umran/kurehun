967aca76dc4f5a476e7899a78215d113d7ef040e
Umran Hussein <umran.hussein@gmail.com>
2016-01-05 17:29:40 -0800

    test

--- routes/auth/flickr.js
+++ routes/auth/flickr.js
@@ -20,8 +20,13 @@ module.exports = function(fkConf){
 	  }
 	))
 
-	router.get('/', passport.authenticate('flickr'), function(req, res){
-		if(req.session.fk_access_token){ return res.send('Flickr Authenticated') }
+	router.get('/', function(req, res){
+		if(req.session.fk_access_token){ 
+			return res.send('Flickr Authenticated') 
+		}
+		
+		passport.authenticate('flickr')
+		
 	})
 
 	router.get('/status', passport.authenticate('flickr', {session: false}), function(req, res) {
