3b2e0e69802b5956e9aa370a7f17924228572536
Umran Hussein <umran.hussein@gmail.com>
2016-01-09 20:42:51 -0800

    normal padding

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kurehun', nav: 'Login' })
+  res.render('index', { title: 'Home', nav: 'Holà!' })
 });
 
 module.exports = router
